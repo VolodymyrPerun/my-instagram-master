a4d77588f1867d56eb5bdb566241da1a5137e16d
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2020-02-03 23:57:15 +0200

    Initial commit from Create React App

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import logo from '../logo.svg';
+import '../App.css'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className='container h-flex'>
+                    <a href="#" className='logo'>
+                        <img src=""></img>
+                    </a>
+                </div>
+            </header>
+
+        );
+    }
+}
+
+
