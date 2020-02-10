5f5101bea42db535844a350be3f3ff3895cf5cfe
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2020-02-10 20:16:12 +0200

    Update Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -9,7 +9,7 @@ export default class Posts extends Component {
                     src="https://images.pexels.com/photos/3224155/pexels-photo-3224155.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
                     alt="nyk"/>
                 <Post
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTboVKwPKx2VUI1hT6tfSY7u4Q0fy2INH1UM1csRyp6qdeklQy_"
+                    src="https://images.pexels.com/photos/3597087/pexels-photo-3597087.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
                     alt="flw"/>
             </div>
         )
