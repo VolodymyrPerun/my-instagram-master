4716f7584230c2618a08df0208473d1d571a4048
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2020-02-16 20:11:46 +0200

    Create renderItems method

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -38,18 +38,17 @@ export default class Posts extends Component {
             const {name, altname, photo, src, alt, desc, id} = item;
 
             return (
-                <div className="post">
-                    <User src="https://images.pexels.com/photos/25758/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                          alt="man"
-                          name="Oldman"
+                <div key={id} className="post">
+                    <User src={photo}
+                          alt={altname}
+                          name={name}
                           min/>
-                    <img src={this.props.src} alt={this.props.alt}/>
+                    <img src={src} alt={alt}/>
                     <div className="post__name">
-                        some account
+                        {name}
                     </div>
                     <div className="post__descr">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                        et dolore magna aliqua.
+                        {desc}
                     </div>
                 </div>
             )
@@ -59,12 +58,7 @@ export default class Posts extends Component {
     render() {
         return (
             <div className='left'>
-                {/*<Post*/}
-                {/*    src="https://images.pexels.com/photos/3224155/pexels-photo-3224155.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"*/}
-                {/*    alt="nyk"/>*/}
-                {/*<Post*/}
-                {/*    src="https://images.pexels.com/photos/3597087/pexels-photo-3597087.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"*/}
-                {/*    alt="flw"/>*/}
+                <User/>
             </div>
         )
     }
